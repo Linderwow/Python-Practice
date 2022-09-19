491cefbe030cecb1986a31c697f53498b7845cca
Aleksandr Akulov <alexyiyi92@gmail.com>
2022-09-19 13:35:14 -0400

    Pizza Order Exercise

--- Section 3/RollerCoaster.py
+++ Section 3/RollerCoaster.py
@@ -1,17 +1,25 @@
-print("Welcome to the rollercoaster")
-
+print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
-    if age >= 18:
-        print("Please, pay $12")
-    elif age < 12:
-        print("Please, pay 5$")
-    elif age >= 12 & age <= 18:
-        print("Please, pay 7$")
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("wrong input")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
