c6b5706c331ef470498ded26d0f47501018eae57
Aleksandr Akulov <alexyiyi92@gmail.com>
2022-09-19 13:45:08 -0400

    refactor code for pizza task

--- Section 3/Pizza.py
+++ Section 3/Pizza.py
@@ -9,15 +9,17 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 
 if size == "S":
     bill = 15
+
 elif size == "M":
     bill = 20
-    if add_pepperoni == "Y":
-        bill += 2
+
 elif size == "L":
     bill = 25
-    if add_pepperoni == "Y":
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
         bill += 3
-
 if extra_cheese == "Y":
     bill += 1
 print(int(bill))
